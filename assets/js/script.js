d8d6fd28d0c00c34340e6f8514f2b259d1f9af55
christiaHiraya91 <chris@DESKTOP-V1HFEFT>
2023-05-01 16:37:09 -0300

    quinto commit

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit (event) {
+  event.preventDefault();
+
+  const gender = getSelectedValue('gender');
+  const age = getInputNumberValue('age');
+  const weight = getInputNumberValue('weight');
+  const height = getInputNumberValue('height');
+  const activityLevel = getSelectedValue('activity_level');
+
+  const tmb = Math.round(
+    gender === 'female'
+    ? (655 + (9.6 * weight) + (1.8 * height) - (4.7 *age)) //if (cálculo pronto)
+    : (66 + (13.7 * weight) + (5 * height) - (6.8 *age)) //else
+  );
+
+  const maintenance = Math.round(tmb * Number(activityLevel));
+  const loseWeight = maintenance - 450;
+  const gainWeight = maintenance + 450;
+
+  //resultados da cálculo para exibir na tela
+  const layout = `
+    <div class="result-content">
+      <ul>
+        <li>
+         Your BMR: <strong>${tmb} Calories/Day</strong>.
+        </li>
+        <li>
+          Maintain weight: <strong>${maintenance} Calories/Day</strong>.
+        </li>
+        <li>
+          Weight loss: <strong>${loseWeight} Calories/Day</strong>.
+        </li>
+        <li>
+          Weight gain: <strong>${gainWeight} Calories/Day</strong>.
+        </li>
+      </ul>
+  `;
+
+  const result = document.getElementById('result');
+
+  result.innerHTML = layout; //repassando que o layout deve ser exibido na tela
+}
+
+function getSelectedValue(id) {
+  const select = document.getElementById(id);
+
+  return select.options[select.selectedIndex].value;
+}
+
+function getInputNumberValue(id) {
+  return Number(document.getElementById(id).value);
+}
